c0a63542d10534566f526bd34830f13ac25d894a
shin1x1 <shin1x1@gmail.com>
2014-06-05 22:58:38 +0900

    add sql logger in heroku

---
+++ app/start/heroku.php
@@ -0,0 +1,5 @@
+<?php
+DB::listen(function($sql, $bindings, $time) {
+    file_put_contents('php://stderr', "[SQL] {$sql} in {$time} s\n" .
+        "      bindinds: ".json_encode($bindings)."\n");
+});
